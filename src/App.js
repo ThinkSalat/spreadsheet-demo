193a1318c0183e330ab0222c62cb09759278b017
Shawn Salat <shawn@trayt.io>
2020-04-22 00:24:56 -0700

    infinite loop caused by renderCell function using conditional and return div

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import ReactDataSheet from 'react-datasheet';
 import 'react-datasheet/lib/react-datasheet.css';
 
 function isValidEmail (email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.trim())
 }
 
 function App() {
 
 	const data = [
-		[
-			{ value: 'First Name', disableEvents: true, isValid: true  },
-			{ value: 'Last Name', disableEvents: true, isValid: true  },
-			{ value: 'Email', disableEvents: true, isValid: true  },
-		],
-		[{}, {}, {}]
+		// [
+		// 	{ value: 'First Name', disableEvents: true, isValid: true  },
+		// 	{ value: 'Last Name', disableEvents: true, isValid: true  },
+		// 	{ value: 'Email', disableEvents: true, isValid: true  },
+		// ],
+		[{value: 'edit one of these'}, {value: 'edit one of these'}, {value: 'edit one of these'}]
 	]
 
 	const [grid, setGrid] = useState(data)
@@ -29,23 +29,17 @@ function App() {
 
 	const renderCell = cell => {
 		console.count('rendering cell')
-		const { value, isValid = true } = cell
-		const color = isValid ? 'black' : 'red'
-		const style = {
-			color
-
-		}
-		return <div style={style}>{value}{!isValid && '*'}</div>
+		if (true) return <div>{cell.value}</div>
 	}
 
 	// on cell deselect, validate contents
 	const handleChange = changes => {
 		const gridCopy = cloneDeep(grid)
 		changes.forEach( ({ cell, row, col, value }) => {
-			const type = col < 2 ? 'name' : 'email'
-			const isValid = validate(value, type)
-			if (!isValid) setContainsErrors(true)
-			gridCopy[row][col] = {...grid[row][col], value, isValid }
+			// const type = col < 2 ? 'name' : 'email'
+			// const isValid = validate(value, type)
+			// if (!isValid) setContainsErrors(true)
+			gridCopy[row][col] = {...grid[row][col], value: value.trim() }
 		})
 		setGrid(gridCopy)
 	}
@@ -67,4 +61,34 @@ function App() {
   );
 }
 
-export default App;
+class App2 extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      grid: [
+        [{value:  1}, {value:  3}],
+        [{value:  2}, {value:  4}]
+      ]
+    }
+  }
+  render () {
+    return (
+      <ReactDataSheet
+        data={this.state.grid}
+        valueRenderer={(cell) => {
+					console.log('rendering')
+					if (true) return cell.value
+				}}
+        onCellsChanged={changes => {
+          const grid = this.state.grid.map(row => [...row])
+          changes.forEach(({cell, row, col, value}) => {
+            grid[row][col] = {...grid[row][col], value}
+          })
+          this.setState({grid})
+        }}
+      />
+    )
+  }
+}
+
+export default App
